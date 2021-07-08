1824d15337e6cca1cd108122f76340df1701d8b9
vladkytsevolov <kytsevolov@gmail.com>
2021-07-07 21:09:41 -0600

    tailwind setup and clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="App-header text-xl">hello </header>
+      <button className="p-2 mt-2 border rounded-md p">Click</button>
+      <span className=""></span>
     </div>
   );
 }
